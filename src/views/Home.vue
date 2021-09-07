cc1d57334595dc8552205255a2f38211ad9e4b45
Lukas Vis <Lukas.Vismn@gmail.com>
2021-09-07 18:45:34 +0200

    cleanup + axios and pagination modules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Previous</p>
+    <p>Next</p>
+  </div>
+  <div>Home</div>
+  <div>
+    PAG
+    <pagination
+      v-model="currentPage"
+      :records="totalAmountOfCards"
+      :per-page="cardsPerPage"
+      @paginate="myCallback"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+/* eslint-disable quotes */
+import Pagination from "v-pagination-3";
+import axios from "axios";
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld,
+    Pagination,
+  },
+  data() {
+    return {
+      cardsPerPage: 10,
+      totalAmountOfCards: 0,
+      currentPage: 1,
+    };
+  },
+  async mounted() {
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?limit=${this.cardsPerPage}`,
+    );
+    const results = res.data;
+    console.log(results);
+    this.totalAmountOfCards = results.count;
+    console.log(this.pageCount);
+  },
+  methods: {
+    clickCallback(page) {
+      console.log(page);
+    },
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.totalAmountOfCards / this.cardsPerPage);
+    },
   },
 };
 </script>
